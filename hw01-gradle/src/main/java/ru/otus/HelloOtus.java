8ed5e2203e49f8f30422a802c9efbff792944bae
Serg <don.tyt@yandex.ru>
2024-01-22 16:58:42 +0300

    checked

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class HelloOtus {
     public static void main(String... args) {
         List<Integer> example = new ArrayList<>();
-        int min=   0;
+        int min = 0;
         int max = 100;
         for (int i = min; i < max; i++) {
             example.add(i);
